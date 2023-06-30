f4dd34cd1e2f035d9891b0c55510f24112da0cc3
huyhieple <huyhiep2625@gmail.com>
2023-06-30 23:01:57 +0700

    sua

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace Quiz_app.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap blob {
+            get {
+                object obj = ResourceManager.GetObject("blob", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap mmh {
             get {
                 object obj = ResourceManager.GetObject("mmh", resourceCulture);
